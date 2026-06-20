1e987a9b886218ab5c4d2e27cfc515a3b97dcf29
Tamires Pessoa <tsousa_14@hotmail.com>
2026-06-20 13:12:52 -0300

    adiciona redes sociais no rodapé

--- frontend/src/components/SiteFooter.jsx
+++ frontend/src/components/SiteFooter.jsx
@@ -22,15 +22,42 @@ export default function SiteFooter() {
             <h4>Navegação</h4>
             <Link to="/site">Início</Link>
             <Link to="/site/imoveis">Imóveis</Link>
-            <Link to="/site/cadastrar-imovel">Cadastre seu imóvel</Link>
+            <Link to="/site/cadastrar-imovel">
+              Cadastre seu imóvel
+            </Link>
           </div>
 
           <div>
             <h4>Contato</h4>
-            <a href="https://wa.me/5511983416160" target="_blank" rel="noreferrer">
-              WhatsApp
+
+            <a
+              href="https://wa.me/5511983416160"
+              target="_blank"
+              rel="noreferrer"
+            >
+              WhatsApp: (11) 98341-6160
             </a>
-            <a href="tel:+5511983416160">(11) 98341-6160</a>
+
+            <a href="mailto:imobiliaria@jbpessoaimoveis.com.br">
+              imobiliaria@jbpessoaimoveis.com.br
+            </a>
+
+            <a
+              href="https://www.instagram.com/jbpessoaimoveis"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram: @jbpessoaimoveis
+            </a>
+
+            <a
+              href="https://www.facebook.com/profile.php?id=61588653701470"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook: JB Pessoa Imóveis
+            </a>
+
             <span>São Paulo - SP</span>
           </div>
         </div>
